f272f5556eaa92160a4db7b919c23aac55db0c8e
nogadas-vonryan <vonnogadas251@gmail.com>
2024-06-10 16:57:02 +0800

    add constants to store html elements in js script

---
+++ nogadas_vonryan/scripts/games.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+const inputId = document.querySelector('#game_id');
+const inputTitle = document.querySelector('#title');
+const inputDescription = document.querySelector('#description');
+const inputGenre = document.querySelector('#genre');
+const inputDeveloper = document.querySelector('#developer');
+const inputDate = document.querySelector('#release_date');
