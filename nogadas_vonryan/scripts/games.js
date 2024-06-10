9173352f662df613454290575b8511405c9e352c
nogadas-vonryan <vonnogadas251@gmail.com>
2024-06-10 16:59:18 +0800

    add function to select and get games from the endpoint

--- nogadas_vonryan/scripts/games.js
+++ nogadas_vonryan/scripts/games.js
@@ -5,3 +5,27 @@ const inputDescription = document.querySelector('#description');
 const inputGenre = document.querySelector('#genre');
 const inputDeveloper = document.querySelector('#developer');
 const inputDate = document.querySelector('#release_date');
+
+const endpoint = '';
+
+async function getGames() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+
+    container.innerHTML = '';
+
+    for(item of data) {
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${item.id}</td>
+            <td>${item.title}</td>
+            <td>${item.description}</td>
+            <td>${item.genre}</td>
+            <td>${item.developer}</td>
+            <td>${item.release_date}</td>`;
+
+        container.append(row);
+    }
+}
