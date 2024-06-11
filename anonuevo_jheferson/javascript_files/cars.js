232fcb0f2273ee6e84a64b06ae18fbccc40406fb
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-06-11 18:39:24 +0800

    create an insert method

--- anonuevo_jheferson/javascript_files/cars.js
+++ anonuevo_jheferson/javascript_files/cars.js
@@ -7,3 +7,22 @@ const inputColor = document.querySelector('#car_color');
 const inputSeats = document.querySelector('#no_of_seats');
 
 const endpoint = '';
+
+async function insertCar() {
+  const options = {
+      method: 'POST',
+      headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+      },
+      body: `car_brand=${inputBrand.value}&\
+          car_model=${inputModel.value}&\
+          car_year=${inputYear.value}&\
+          car_color=${inputColor.value}&\
+          no_of_seats=${inputSeats.value}`
+  };
+
+  const response = await fetch(endpoint, options);
+  const data = await response.text();
+
+  getCars();
+}
