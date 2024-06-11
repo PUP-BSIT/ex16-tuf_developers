cdb9db6bae4ac5c3938e9197ecdde74ad696404d
mauroche <rochemaui165@gmail.com>
2024-06-12 00:36:43 +0800

    update php config

--- roche_mauijane/crud/movie.php
+++ roche_mauijane/crud/movie.php
@@ -4,9 +4,9 @@ header("Access-Control-Allow-Methods: GET, POST, DELETE, PATCH, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 $server = 'localhost';
-$username = 'u423230023_jhef_anon_01';
-$password = 'JHEFtufhostinger1234';
-$db = 'u423230023_exercise_16';
+$username = $_ENV['username'];
+$password = $_ENV['password'];
+$db = 'movie';
 
 $conn = mysqli_connect($server, $username, $password, $db);
 
