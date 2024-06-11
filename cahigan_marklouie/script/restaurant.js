16c8e3e1bc995e4bb10606694c76f7d466ee0b60
cahigan-marklouie <markcahigan54@gmail.com>
2024-06-11 18:53:47 +0800

    assign javascript variables in restaurant.js

---
+++ cahigan_marklouie/script/restaurant.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+const inputId = document.querySelector('#restaurant_id');
+const inputRestaurantName = document.querySelector('#restaurant_name');
+const inputLocation = document.querySelector('#location');
+const inputFavoriteFood = document.querySelector('#favorite_food');
+const inputOperatingHours = document.querySelector('#operating_hours');
+const inputContactInfo = document.querySelector('#contact_info');
