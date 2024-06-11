975de42d068a007735cc1f9788394a68b58c131e
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-06-11 22:00:56 +0800

    add php header

--- anonuevo_jheferson/php_files/cars.php
+++ anonuevo_jheferson/php_files/cars.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PATCH, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 $server = 'localhost';
 $username = 'root';
 $password = '';
