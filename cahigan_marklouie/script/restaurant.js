952e3e4599f6e6780ff05b7316f13ba6da137ff2
cahigan-marklouie <markcahigan54@gmail.com>
2024-06-11 21:27:17 +0800

    add javascript variable in restaurant.js

--- cahigan_marklouie/script/restaurant.js
+++ cahigan_marklouie/script/restaurant.js
@@ -5,3 +5,5 @@ const inputLocation = document.querySelector('#location');
 const inputFavoriteFood = document.querySelector('#favorite_food');
 const inputOperatingHours = document.querySelector('#operating_hours');
 const inputContactInfo = document.querySelector('#contact_info');
+
+const endpoint = 'http://localhost/test/exercise16.php';
