d16f8f9a25949aeba9c3742a60b570c24cd15d7b
mauroche <rochemaui165@gmail.com>
2024-06-11 22:37:41 +0800

    create insert method to movie.js

--- roche_mauijane/crud/movie.js
+++ roche_mauijane/crud/movie.js
@@ -7,3 +7,30 @@ const inputDirector = document.querySelector('#director');
 const inputReleaseDate = document.querySelector('#release_date');
 
 const endpoint = 'http://localhost/php_file/movie.php';
+
+async function getMovie() {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  container.innerHTML = '';
+
+  for (const item of data) {
+      const row = document.createElement('tr');
+      const editButton = getEditButton(item);
+      const deleteButton = getDeleteButton(item);
+
+      row.innerHTML = `
+          <td>${item.id}</td>
+          <td>${item.title}</td>
+          <td>${item.movie_description}</td>
+          <td>${item.genre}</td>
+          <td>${item.director}</td>
+          <td>${item.release_date}</td>`;
+
+      row.append(editButton);
+      row.append(deleteButton);
+      container.append(row);
+  }
+
+  setInputs();
+}
