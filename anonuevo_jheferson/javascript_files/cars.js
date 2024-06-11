373a49611acf4189b4b6a442216da76b2ba80bed
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-06-11 18:41:05 +0800

    create a delete button function

--- anonuevo_jheferson/javascript_files/cars.js
+++ anonuevo_jheferson/javascript_files/cars.js
@@ -61,3 +61,14 @@ async function deleteCar(id) {
 
   getCars();
 }
+
+function getDeleteButton(item) {
+  const cell = document.createElement('td');
+  const button = document.createElement('button');
+
+  button.addEventListener('click', deleteCar.bind(null, item.id));
+
+  button.textContent = 'Delete';
+  cell.append(button);
+  return cell;
+}
