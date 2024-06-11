0f98857c9c2ba2f2c6ad921ebcdae46919c42942
cahigan-marklouie <markcahigan54@gmail.com>
2024-06-11 21:29:57 +0800

    add function getRestaurant in restaurant.js

--- cahigan_marklouie/script/restaurant.js
+++ cahigan_marklouie/script/restaurant.js
@@ -7,3 +7,30 @@ const inputOperatingHours = document.querySelector('#operating_hours');
 const inputContactInfo = document.querySelector('#contact_info');
 
 const endpoint = 'http://localhost/test/exercise16.php';
+
+async function getRestaurant() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    container.innerHTML = '';
+
+    for(item of data) {
+        const row = document.createElement('tr');
+        const editButton = getEditButton(item);
+        const deleteButton = getDeleteButton(item);
+
+        row.innerHTML = `
+            <td>${item.id}</td>
+            <td>${item.restaurant_name}</td>
+            <td>${item.location}</td>
+            <td>${item.favorite_food}</td>
+            <td>${item.operating_hours}</td>
+            <td>${item.contact_info}</td>`;
+
+        row.append(editButton);
+        row.append(deleteButton);
+        container.append(row);
+    }
+
+    setInputs();
+}
