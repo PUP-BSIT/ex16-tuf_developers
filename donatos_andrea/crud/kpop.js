edb3989bd024edd922f476dfb8ba03e3f1a50ef4
Andreadonatos <dreadonatos@gmail.com>
2024-06-11 20:29:16 +0800

    add javascript file

---
+++ donatos_andrea/crud/kpop.js
@@ -0,0 +1,127 @@
+const container = document.querySelector('#container');
+const inputId = document.querySelector('#kpop_id');
+const inputGroup = document.querySelector('#group');
+const inputSong = document.querySelector('#song');
+const inputBias = document.querySelector('#bias');
+const inputDebut = document.querySelector('#debut');
+const inputCompany = document.querySelector('#company');
+
+const endpoint = 'http://localhost/test/kpop.php';
+
+async function getKpop() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    container.innerHTML = '';
+
+    for (const item of data) {
+        const row = document.createElement('tr');
+        const editButton = getEditButton(item);
+        const deleteButton = getDeleteButton(item);
+
+        row.innerHTML = `
+            <td>${item.id}</td>
+            <td>${item.group}</td>
+            <td>${item.song}</td>
+            <td>${item.bias}</td>
+            <td>${item.debut}</td>
+            <td>${item.company}</td>`;
+
+        row.append(editButton);
+        row.append(deleteButton);
+        container.append(row);
+    }
+
+    setInputs();
+    console.log(data);
+}
+
+async function insertKpop() {
+    const options = {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded",
+        },
+        body: `group=${inputGroup.value}&\
+            song=${inputSong.value}&\
+            bias=${inputBias.value}&\
+            debut=${inputDebut.value}&\
+            company=${inputCompany.value}`
+    };
+
+    const response = await fetch(endpoint, options);
+    const data = await response.text();
+
+    getKpop();
+}
+
+async function updateKpop() {
+    const options = {
+        method: 'PATCH',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded",
+        },
+        body: `id=${inputId.value}&\
+            group=${inputGroup.value}&\
+            song=${inputSong.value}&\
+            bias=${inputBias.value}&\
+            debut=${inputDebut.value}&\
+            company=${inputCompany.value}`
+    };
+
+    const response = await fetch(endpoint, options);
+    const data = await response.text();
+
+    getKpop();
+}
+
+async function deleteKpop(id) {
+    const options = {
+        method: 'DELETE',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded",
+        },
+        body: `id=${id}`
+    };
+
+    const response = await fetch(endpoint, options);
+    const data = await response.text();
+
+    getKpop();
+}
+
+function getDeleteButton(item) {
+    const cell = document.createElement('td');
+    const button = document.createElement('button');
+
+    button.addEventListener('click', deleteCar.bind(null, item.id));
+
+    button.textContent = 'Delete';
+    cell.append(button);
+    return cell;
+}
+
+function getEditButton(item) {
+    const cell = document.createElement('td');
+    const button = document.createElement('button');
+
+    button.addEventListener('click', setInputs.bind(null, item.id, 
+      item.group, item.song, item.bias, item.debut,
+      item.company));
+
+    button.textContent = 'Edit';
+    cell.append(button);
+
+    return cell;
+}
+
+function setInputs(id, group, song, bias, color, company) {
+    inputId.value = id ?? '';
+    inputGroup.value = group ?? '';
+    inputSong.value = song ?? '';
+    inputBias.value = bias ?? '';
+    inputDebut.value = color ?? '';
+    inputCompany.value = company ?? '';
+}
+
+getKpop();
