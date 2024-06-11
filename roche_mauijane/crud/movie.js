585e2b7baf095eaa80377897adb4cca7d370c1da
mauroche <rochemaui165@gmail.com>
2024-06-11 22:34:17 +0800

    assign javascript variables

---
+++ roche_mauijane/crud/movie.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+const inputId = document.querySelector('#movie_id');
+const inputTitle = document.querySelector('#title');
+const inputMovieDescription = document.querySelector('#movie_description');
+const inputGenre = document.querySelector('#genre');
+const inputDirector = document.querySelector('#director');
+const inputReleaseDate = document.querySelector('#release_date');
