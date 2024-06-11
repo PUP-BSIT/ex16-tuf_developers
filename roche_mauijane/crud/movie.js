7d19e51c58b54c39042ba2203ed6263ed647d37f
mauroche <rochemaui165@gmail.com>
2024-06-11 22:35:28 +0800

    add endpoint to movie.js

--- roche_mauijane/crud/movie.js
+++ roche_mauijane/crud/movie.js
@@ -5,3 +5,5 @@ const inputMovieDescription = document.querySelector('#movie_description');
 const inputGenre = document.querySelector('#genre');
 const inputDirector = document.querySelector('#director');
 const inputReleaseDate = document.querySelector('#release_date');
+
+const endpoint = 'http://localhost/php_file/movie.php';
