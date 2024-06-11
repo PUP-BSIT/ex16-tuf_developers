0eeb932a63b03e5bb00dab25044204bc6239feb2
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-06-11 16:51:40 +0800

    add endpoint to cars.js

--- anonuevo_jheferson/javascript_files/cars.js
+++ anonuevo_jheferson/javascript_files/cars.js
@@ -1,6 +1,9 @@
+const container = document.querySelector('#form_container');
 const inputId = document.querySelector('#car_id');
 const inputBrand = document.querySelector('#car_brand');
 const inputModel = document.querySelector('#car_model');
 const inputYear = document.querySelector('#car_year');
 const inputColor = document.querySelector('#car_color');
 const inputSeats = document.querySelector('#no_of_seats');
+
+const endpoint = 'http://localhost/test/exercise16.php';
