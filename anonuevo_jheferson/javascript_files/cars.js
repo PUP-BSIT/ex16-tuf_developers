ba530392e61cd3ac52d5b48ec7c9da184502b641
anonuevo-jheferson <jhefersonzanonuevo@gmail.com>
2024-06-11 16:46:14 +0800

    assign javascript variables

---
+++ anonuevo_jheferson/javascript_files/cars.js
@@ -0,0 +1,6 @@
+const inputId = document.querySelector('#car_id');
+const inputBrand = document.querySelector('#car_brand');
+const inputModel = document.querySelector('#car_model');
+const inputYear = document.querySelector('#car_year');
+const inputColor = document.querySelector('#car_color');
+const inputSeats = document.querySelector('#no_of_seats');
